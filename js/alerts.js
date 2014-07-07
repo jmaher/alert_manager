f1717d9fde12832e348b95dd4ed2920d31446f01
Joel Maher <joel.maher@gmail.com>
2014-07-07 16:57:35 +0000

    Bug 1034109 - [UI] Alert Manager needs a new page to view expired alerts

---
+++ js/alerts.js
@@ -0,0 +1,277 @@
+var root_url = 'http://54.215.155.53:8080';
+
+$.getJSON( root_url + "/data/getvalues", function( data ) {
+    
+    var tests = data['test'];
+    var revs = data['rev'];
+    var platforms = data['platform'];
+    if (parseInt(results['showAll']) == 1) {
+        document.getElementById("checkbox").checked = true;
+    }
+    
+    for(var i in tests) {
+        var newoption = document.createElement("option");
+         newoption.id = "test";
+         var value = tests[i][0];
+            $("#test").append("<option value=\""+value+"\">"+value+"</option>");
+     }
+
+    for(i in revs) {
+        var newoption = document.createElement("option");
+         newoption.id = "rev";
+         var value = revs[i][0];
+            $("#rev").append("<option value=\""+value+"\">"+value+"</option>");
+    }
+
+    for(i in platforms) {
+        var newoption = document.createElement("option");
+         newoption.id = "platform";
+         var value = platforms[i][0];
+            $("#platform").append("<option value=\""+value+"\">"+value+"</option>");
+    }
+                
+    document.getElementById("rev").value = results['rev'];
+    document.getElementById("test").selectedIndex = results['testIndex'];
+    document.getElementById("platform").selectedIndex = results['platIndex'];
+
+});
+
+$(document).ready(function() { 
+                   
+                    $('#button').click(function() { 
+                            var rev = $('#rev').val();
+                            var test = $('#test').val();
+                            var platform = $('#platform').val();
+                            var showall = 0;
+                            var testIndex = $("select[id='test'] option:selected").index();
+                            var platIndex = $("select[id='platform'] option:selected").index();
+                            if($('#checkbox').is(":checked")) {
+                                console.log("checked");
+                                showall = 1;
+                            }
+                         document.cookie="platform = "+platform;
+                         document.cookie="test = "+test;                           
+                         location.href = "?rev="+rev+"&showAll="+showall+"&testIndex="+testIndex+"&platIndex="+platIndex;
+                                    
+                    });
+});
+
+
+function hideMerged(originalkeyrev, showall) {
+    var req = new XMLHttpRequest();
+    req.onload = function(e) {
+        var raw_data = JSON.parse(req.response);
+
+        var fields = ["date", "branch", "test", "platform", "percent", "graphurl", "changeset", "tbplurl", "comment", "bug", "status"]
+        var alerts = raw_data.alerts;   
+     
+        var keyrev = "";
+        var tbl = "";
+        // insert revisions into lower table
+        for (var alert in alerts) {
+            if (alerts[alert]["mergedfrom"] == originalkeyrev) {
+                tbl = document.getElementById(originalkeyrev + "-tbl");
+            } else {
+                continue;
+            }
+            var row = $("#" + alerts[alert]["id"] + "-" + originalkeyrev);
+            if (row) {
+                row.remove();
+            }
+        }
+        var mergedfromhtml = "<span id=\"mergedfrom-" + originalkeyrev + "\" onclick=\"showMerged('" + originalkeyrev + "', " + showall + ");\">view merged alerts</span>";
+        
+          $("#" + originalkeyrev + "-hdr").html("<a href=?rev="+originalkeyrev+"&showall=1&testIndex=0&platIndex=0><h3>" + originalkeyrev + "</h3></a>" + mergedfromhtml);  
+    }
+    req.open('get', root_url + '/data/mergedalerts?keyrev=' + originalkeyrev, true);
+    req.send();
+}
+
+function showMerged(originalkeyrev, showall) {
+    var req = new XMLHttpRequest();
+    req.onload = function(e) {
+        var raw_data = JSON.parse(req.response);
+        var alerts = raw_data.alerts;   
+     
+        var tbl = "";
+        // insert revisions into lower table
+        for (var alert in alerts) {
+            if (alerts[alert]["mergedfrom"] != originalkeyrev) {
+                continue;
+            }
+            tbl = document.getElementById(originalkeyrev + "-tbl");
+            addAlertToUI(tbl, alerts[alert], showall, originalkeyrev);
+        }
+        var mergedfromhtml = "<span id=\"mergedfrom-" + originalkeyrev + "\" onclick=\"hideMerged('" + originalkeyrev + "', " + showall + ");\">hide merged alerts</span>";
+       
+          $("#" + originalkeyrev + "-hdr").html("<h3><a href=?rev="+originalkeyrev+"&showall=1&testIndex=0&platIndex=0>" + originalkeyrev + "</a></h3>" + mergedfromhtml);
+    }
+    req.open('get', root_url + '/data/mergedalerts?keyrev=' + originalkeyrev, true);
+    req.send();
+}
+
+function addMergedLinks(showall) {
+    var req = new XMLHttpRequest();
+    req.onload = function(e) {
+        var raw_data = JSON.parse(req.response);
+
+        var fields = ["id", "date", "bug", "status", "keyrevision", "bugcount", "mergedfrom"]
+        var alerts = raw_data.alerts;   
+        
+        var count = 0;
+        for (var alert in alerts) {
+            if (alerts[alert]['mergedfrom'] != '') {
+                var mf = alerts[alert]['mergedfrom'];
+                if ($("#" + mf + "-hdr").html() == "") {
+                    continue;
+                }
+
+                var mergedfromhtml = "<span id=\"mergedfrom-" + mf + "\" onclick=\"showMerged('" + mf + "', " + showall + ");\">view merged alerts</span>";
+                
+                  $("#" + mf + "-hdr").html("<a href=?rev="+mf+"&showall=1&testIndex=0&platIndex=0><h3>" + mf + "</a></h3>" + mergedfromhtml);
+            }
+        }
+    }
+    req.open('get', root_url + '/data/mergedids', true);
+    req.send();
+}
+
+function updateStatus(alertid, duplicate, bugid, mergedfrom) {
+    var status = $("#" + alertid + "-status").val();
+    if (status == 'Duplicate') {
+        // popup window with field for duplicate, seeded with alert['duplicate']
+        // consider merged rev if needed
+        suggestedDuplicate = duplicate;
+        if (suggestedDuplicate == 'null' || suggestedDuplicate == '') {
+            suggestedDuplicate = mergedfrom;
+        }
+
+        AddDuplicateUI.openDuplicateBox(alertid, suggestedDuplicate);
+    } else if (status == 'Backout') {
+        var bug = bugid;
+        if (bug == '') {
+            bug = $("#" + alertid + "-bug").val();
+        }
+        AddBugUI.openBugBox(alertid, bug, 'Backout');
+    } else {
+        $.ajax({
+          url: "data/updatestatus",
+          type: "POST",
+          data: {
+            id: alertid,
+            status: status,
+          }
+        });
+    }
+}
+
+function updateBug(alertid, bugid, status) {
+    var bug = bugid;
+    if (bug == '') {
+        bug = $("#" + alertid + "-bug").val();
+    }
+    if (status == '') {
+        status = 'NEW';
+    }
+
+    AddBugUI.openBugBox(alertid, bug, status);
+}
+
+function updateTbplURL(alertid, tbplurl) {
+    AddTbplUI.openTbplBox(alertid, tbplurl);
+}
+
+function addAlertToUI(tbl,alert,showall){
+    addMergedAlertToUI(tbl, alert, showall, "");
+}
+
+
+// Function idDescending sorts the objects in the descending order of their id. This way, we can view the most recent alerts at the top.
+// The objects have been sorted based on their id and not on their date as sorting by the date field was not working.
+function idDescending(a, b) {
+    if (a["id"] < b["id"]) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
+function loadAllAlerts(showall, rev, test, platform) {
+    var req = new XMLHttpRequest();
+    req.onload = function(e) {
+        var raw_data = JSON.parse(req.response);
+        var alerts = raw_data.alerts;   
+        alerts.sort(idDescending);
+
+        var keyrev = "";
+        var tbl = "";
+        // insert revisions into lower table
+        for (var alert in alerts) {
+            if (alerts[alert]["keyrevision"] != keyrev) {
+                keyrev = alerts[alert]["keyrevision"];
+                if ($("#" + keyrev + "-hdr").html() == null) {
+                    var newdiv = document.createElement("div");
+                    newdiv.id = keyrev;
+                    $("#revisions").append(newdiv);
+                   
+                    $("#" + keyrev).append("<span id=\"" + keyrev + "-hdr\"><a href=?rev="+keyrev+"&showall=1&testIndex=0&platIndex=0><h3>" + keyrev + "</h3></a></span>");
+                      
+                }
+                if ($("#" + keyrev + "-tbl").html() == null) {
+                    var kdiv = document.getElementById(keyrev);
+                    var newtbl = document.createElement("table");
+                    newtbl.id = keyrev + '-tbl';
+                    $("#" + keyrev).append(newtbl);
+                }
+                
+                 $("#" + keyrev + "-hdr").html("<a href=?rev="+keyrev+"&showall=1&testIndex=0&platIndex=0><h3>" + keyrev + "</h3></a>");
+                tbl = document.getElementById(keyrev + "-tbl");
+            }
+            var r = addAlertToUI(tbl, alerts[alert], showall);
+            if ($("#" + keyrev + '-tbl tr').size() == 0) {
+                $("#" + keyrev + "-hdr").html("");
+            }
+        }
+        addMergedLinks(showall);
+        AddCommentUI.init();
+        AddDuplicateUI.init();
+        AddBugUI.init();
+        AddTbplUI.init();
+    }
+    req.open('get', (root_url + '/data/alertsbyrev?rev='+rev+'&test='+test+'&platform='+platform), true);
+    req.send();
+}
+
+function editAlert(id, body) {
+    return function() {
+      AddCommentUI.openCommentBox(id, body);
+    }
+}
+
+function hideDiv(name) {
+    document.getElementById(name).style.display = "none";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+ 
+function getJsonFromUrl() {
+  var query = location.search.substr(1);
+  var data = query.split("&");
+  var result = {};
+  for(var i=0; i<data.length; i++) {
+    var item = data[i].split("=");
+    result[item[0]] = item[1];
+  }
+  return result;
+}
+
+
